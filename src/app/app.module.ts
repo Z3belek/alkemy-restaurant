641229a97a931ae10ef75d88095cb299b7c77079
Zebelek <you@example.com>
2022-09-06 16:50:22 -0300

    Update auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -19,7 +21,11 @@ import { SearchComponent } from './pages/search/search.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  urlAuth = 'http://challenge-react.alkemy.org/';
+  authToken!: string;
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  constructor() { }
+  login(authData: AuthData): Observable<any> {
+    return this.http.post<any>(this.urlAuth, authData).pipe(map(data => {
+      localStorage.setItem('authToken', JSON.stringify(data));
+      return data;
+    }))
+  }
+
+  getToken(): boolean {
+    return (localStorage.getItem('authToken') !== null);
+  }
+
+  logout(): void {
+    localStorage.removeItem('authToken');
+  }
+}
+
+export interface AuthData {
+  email: string;
+  password: string;
 }

62f1b50c52966dd176add0786649dba20162ad13
Zebelek <you@example.com>
2022-09-06 21:25:46 -0300

    Login with swal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoggedGuard } from './guards/logged.guard';
+import { LoginComponent } from './pages/login/login.component';
 
-
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [LoggedGuard]
+  }
+]
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm!: FormGroup;
+  hide = true;
+  btnDisabled: boolean = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['' , [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]]
+    });
+  }
+
+  login(event: Event) {
+    event.preventDefault;
+    this.btnDisabled = true;
+    if(this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe({
+        next: (res) => {
+          setTimeout(() => {
+            this.btnDisabled = false;
+          }, 1500);
+          this.router.navigateByUrl('home');
+        },
+        error: (err: HttpErrorResponse) => {
+          this.btnDisabled = false;
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Email or password incorrect!',
+          })
+        }
+      })
+    }
   }
 
 }
